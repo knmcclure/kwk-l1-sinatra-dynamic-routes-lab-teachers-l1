07d2ad6802b2f94a4da3e11b3fa82a18b0a9733c
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-06-27 00:12:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    @number = @number ** 2
+    @number.to_s
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
+  end
 
 end
